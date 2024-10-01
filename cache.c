149291651f1eeb3c4c2cae767be75639e960f623
hamzabdel <hamzaabdel@csu.fullerton.edu>
2024-09-30 22:48:09 -0700

    working on LFU

--- cache.c
+++ cache.c
@@ -13,24 +13,30 @@ int cache_hits = 0;
 int cache_misses = 0;
 
 void init_cache() {
-    for (int i = 0; i < CACHE_SIZE; i++) {
+    for (int i = 0; i < CACHE_SIZE; i++) 
+    {
         cache[i].number = -1; 
     }
 }
 
 int get_from_cache(int number) {
-    for (int i = 0; i < cache_count; i++) {
-        if (cache[i].number == number) {
+    for (int i = 0; i < cache_count; i++) 
+    {
+        if (cache[i].number == number) 
+        {
             cache_hits++; 
             return cache[i].steps;
         }
     }
+
     cache_misses++; 
     return -1; 
 }
 
-void add_to_cache(int number, int steps) {
-    if (cache_count < CACHE_SIZE) {
+void add_to_cache(int number, int steps) 
+{
+    if (cache_count < CACHE_SIZE) 
+    {
         cache[cache_count].number = number;
         cache[cache_count].steps = steps;
         cache_count++;
@@ -41,7 +47,8 @@ void add_to_cache(int number, int steps) {
     }
 }
 
-float get_cache_hit_percentage() {
+float get_cache_hit_percentage() 
+{
     if (cache_hits + cache_misses == 0) return 0; 
     return (float)cache_hits / (cache_hits + cache_misses) * 100; 
 }
